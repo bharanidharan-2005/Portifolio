bdf35c0a56cd5ecb691c88c7df6b26071236419a
BHARANIDHARAN.M <laughinggupta2025@gmail.com>
2026-07-09 18:25:27 +0530

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -80,3 +80,12 @@ class AISessionLog(models.Model):
             "desc": self.description,
             "status": self.status
         }
+        
+        
+class OTPVerification(models.Model):
+    email = models.EmailField(unique=True)
+    otp_code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.email} - {self.otp_code}"
