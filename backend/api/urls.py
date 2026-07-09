53b2644625585a5f0b4b7e745212f51f540a458d
BHARANIDHARAN.M <laughinggupta2025@gmail.com>
2026-07-09 18:26:25 +0530

    Add OTP verification and image generation endpoints

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
+from . import views
 from .views import (
     PageListAPIView, 
     SectionDetailAPIView, 
     AISectionRefinementView, 
     ResumeUploadAPIView, 
     AITemplateGeneratorView,
-    PortfolioReviewAPIView  # ⚡ Step 1: Make sure this is imported
+    PortfolioReviewAPIView,
+    generate_custom_image # ⚡ Step 1: Make sure this is imported
 )
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
 
 urlpatterns = [
     path('pages/', PageListAPIView.as_view(), name='page-list'),
@@ -14,7 +22,12 @@ urlpatterns = [
     path('ai-refinement/', AISectionRefinementView.as_view(), name='ai-refinement'),
     path('upload-resume/', ResumeUploadAPIView.as_view(), name='upload-resume'),
     path('ai-generate-template/', AITemplateGeneratorView.as_view(), name='ai-generate-template'),
-    
+    path('send-otp/', views.send_verification_otp, name='send_otp'),
+    path('verify-otp/', views.verify_otp_code, name='verify_otp'),
     # ⚡ Step 2: Register the review analytics path endpoint
     path('portfolio-review/', PortfolioReviewAPIView.as_view(), name='portfolio-review'), 
+    path('generate-image/',generate_custom_image, name='generate_custom_image'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
